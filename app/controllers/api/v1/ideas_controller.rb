5fe2e31e183068609396207bce75979b672e885e
ShogoSonoda <sonoponsyougo@gmail.com>
2021-09-06 19:14:39 +0900

    [add]アイデア登録APIのコントローラーの設定

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,2 +1,22 @@
-class IdeasController < ApplicationController
+module Api
+  module V1
+    class IdeasController < ApplicationController
+
+      def create
+        category_name = params[:category_name]
+        category = Category.find_or_create_by(name: category_name)
+        idea = Idea.new(category_id: category.id, body: params[:body])
+        if idea.save
+          render json: { status: 201, data: idea }
+        else
+          render json: { status: 422 }
+        end
+      end
+
+      def index
+        ideas = Idea.order(created_at: :desc)
+        render json: { data: ideas }
+      end
+    end
+  end
 end
