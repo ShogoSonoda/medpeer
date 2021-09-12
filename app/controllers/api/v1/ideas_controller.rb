e4a369adef5132191dca018253d8fb492f88c3c0
ShogoSonoda <sonoponsyougo@gmail.com>
2021-09-12 10:29:49 +0900

    [update]アイデア取得時にcategory_nameによって表示するアイデアをフィルタリングできるようにする

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -11,7 +11,18 @@ class Api::V1::IdeasController < ApplicationController
   end
 
   def index
-    ideas = Idea.all
-    render json: ideas, each_serializer: IdeaSerializer, root: "data"
+    category_name = params[:category_name]
+    if category_name != nil
+      if category = Category.find_by(name: category_name) != nil
+        category_id = Category.find_by(name: category_name).id
+        ideas = Idea.where(category_id: category_id)
+        render json: ideas, each_serializer: IdeaSerializer, root: "data"
+      else
+        render json: { status: 404 }
+      end
+    else
+      ideas = Idea.all
+      render json: ideas, each_serializer: IdeaSerializer, root: "data"
+    end
   end
 end
